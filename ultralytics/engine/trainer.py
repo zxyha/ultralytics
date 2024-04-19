8e9a0a6d7b323de9c91a7ea4fccf90639e4ec0ca
zhaoxianyou <zxy@ya.com>
2024-04-19 11:06:27 +0800

    修改训练结束时精简模型的保存路径

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -630,7 +630,13 @@ class BaseTrainer:
         """Performs final evaluation and validation for object detection YOLO model."""
         for f in self.last, self.best:
             if f.exists():
-                strip_optimizer(f)  # strip optimizers
+                # custom modified
+                file_path = f if isinstance(f, str) else f.parent
+                folder_path, file_name = os.path.split(file_path)
+                save_path = os.path.join(folder_path, file_name.split('.')[0] + '_strip.' + file_name.split('.')[1])
+                strip_optimizer(f,save_path)  # strip optimizers
+                # custom modified
+                #strip_optimizer(f)
                 if f is self.best:
                     LOGGER.info(f"\nValidating {f}...")
                     self.validator.args.plots = self.args.plots
